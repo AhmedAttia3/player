662cffcc6fb3d51245a78c6a1618ca2fe5c2bde6
ahmed <aatia27@gmail.com>
2020-11-12 13:57:26 +0200

    remove urls

--- mytestapplication/src/main/java/com/example/mytestapplication/MainActivity.java
+++ mytestapplication/src/main/java/com/example/mytestapplication/MainActivity.java
@@ -13,9 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        VideoActivity.intentTo(this, "http://apk2tv.zalhd.net:20002/live/ZHD13/ZHD13/224.ts", "f");
         ijkVideoView = findViewById(R.id.video_view);
-        ijkVideoView.setVideoPath("http://apk2tv.zalhd.net:20002/live/ZHD13/ZHD13/224.ts");
+        ijkVideoView.setVideoPath("");//video url
         ijkVideoView.start();
     }
 
